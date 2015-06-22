851fdfcba8df2a700c8e8db34e237a7a824b0de9
Stefano Dacchille <stefano.dacchille@gmail.com>
2015-06-22 07:46:37 +0200

    Add tests for binary operations

--- app/src/test/kotlin/com/stefanodacchille/playground/ReactiveModelSpecs.kt
+++ app/src/test/kotlin/com/stefanodacchille/playground/ReactiveModelSpecs.kt
@@ -10,14 +10,19 @@ class ReactiveModelSpecs : Spek() {
       var currentState: State = State.initialState
       ReactiveModel.updateObservable.subscribe { state -> currentState = state }
 
-      on("updating the state by adding digits") {
-        it("should have correct init state") {
-//          ReactiveModel.actionSubject.onNext(Action.ZERO)
-//          when (currentState) {
-//            is State.Init -> {
-//              assertEquals("0", currentState.displayNumber.toDisplay())
-//            }
-//          }
+      // Doesn't do what I expect
+      //      beforeOn {
+      //        ReactiveModel.actionSubject.onNext(Action.CLEAR)
+      //      }
+
+      on("entering digits") {
+        it("should save the entered number correctly") {
+          //          ReactiveModel.actionSubject.onNext(Action.ZERO)
+          //          when (currentState) {
+          //            is State.Init -> {
+          //              assertEquals("0", currentState.displayNumber.toDisplay())
+          //            }
+          //          }
           ReactiveModel.actionSubject.onNext(Action.ZERO)
           assertEquals("0", (currentState as State.Init).displayNumber.toDisplay())
           Action.digits().foldRight("") { action, displayNumber ->
@@ -30,9 +35,64 @@ class ReactiveModelSpecs : Spek() {
         }
       }
 
-      on("updating the state by applying a binary operation") {
-        it("should have correct operation state") {
-          fail("Not implemented")
+      // TODO: Test this using https://github.com/pholser/junit-quickcheck
+      on("applying a binary operation") {
+        var expectedState: State
+
+        it("should save state correctly") {
+          ReactiveModel.actionSubject.onNext(Action.CLEAR)
+          ReactiveModel.actionSubject.onNext(Action.SIX)
+          ReactiveModel.actionSubject.onNext(Action.ADD)
+          expectedState = State.Operation(6.0, Action.ADD, DisplayNumber.zero)
+          assertEquals(expectedState, currentState)
+
+          ReactiveModel.actionSubject.onNext(Action.THREE)
+          expectedState = State.Operation(6.0, Action.ADD, DisplayNumber(value = "3"))
+          assertEquals(expectedState, currentState)
+        }
+
+        it("should calculate the result of an addition correctly") {
+          ReactiveModel.actionSubject.onNext(Action.CLEAR)
+          ReactiveModel.actionSubject.onNext(Action.SIX)
+          ReactiveModel.actionSubject.onNext(Action.ADD)
+          ReactiveModel.actionSubject.onNext(Action.THREE)
+          ReactiveModel.actionSubject.onNext(Action.EQUALS)
+
+          expectedState = State.Operation(9.0, Action.ADD, DisplayNumber.zero)
+          assertEquals(expectedState, currentState)
+        }
+
+        it("should calculate the result of a subtraction correctly") {
+          ReactiveModel.actionSubject.onNext(Action.CLEAR)
+          ReactiveModel.actionSubject.onNext(Action.THREE)
+          ReactiveModel.actionSubject.onNext(Action.SUB)
+          ReactiveModel.actionSubject.onNext(Action.SIX)
+          ReactiveModel.actionSubject.onNext(Action.EQUALS)
+
+          expectedState = State.Operation(-3.0, Action.SUB, DisplayNumber.zero)
+          assertEquals(expectedState, currentState)
+        }
+
+        it("should calculate the result of a multiplication correctly") {
+          ReactiveModel.actionSubject.onNext(Action.CLEAR)
+          ReactiveModel.actionSubject.onNext(Action.SEVEN)
+          ReactiveModel.actionSubject.onNext(Action.MUL)
+          ReactiveModel.actionSubject.onNext(Action.SIX)
+          ReactiveModel.actionSubject.onNext(Action.EQUALS)
+
+          expectedState = State.Operation(42.0, Action.MUL, DisplayNumber.zero)
+          assertEquals(expectedState, currentState)
+        }
+
+        it("should calculate the result of a division correctly") {
+          ReactiveModel.actionSubject.onNext(Action.CLEAR)
+          ReactiveModel.actionSubject.onNext(Action.SIX)
+          ReactiveModel.actionSubject.onNext(Action.DIV)
+          ReactiveModel.actionSubject.onNext(Action.FIVE)
+          ReactiveModel.actionSubject.onNext(Action.EQUALS)
+
+          expectedState = State.Operation(1.2, Action.DIV, DisplayNumber.zero)
+          assertEquals(expectedState, currentState)
         }
       }
 
@@ -74,7 +134,9 @@ class ReactiveModelSpecs : Spek() {
 
       on("updating the state by clearing it") {
         it("should have a zero init state") {
-          fail("Not implemented")
+          ReactiveModel.actionSubject.onNext(Action.SIX)
+          ReactiveModel.actionSubject.onNext(Action.CLEAR)
+          assertEquals(State.initialState, currentState)
         }
       }
     }
